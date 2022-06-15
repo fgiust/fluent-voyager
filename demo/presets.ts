1958c66096892506964e308dc6f5315bab5280d8
Marco Conti <marco.conti@openmindonline.it>
2022-06-15 15:27:34 +0200

    fluent introspection schema

--- demo/presets.ts
+++ demo/presets.ts
@@ -1,14 +1,8 @@
-const githubIntrospection = require('./presets/github_introspection.json');
-const swapiIntrospection = require('./presets/swapi_introspection.json');
-const yelpIntrospection = require('./presets/yelp_introspection.json');
-const shopifyIntrospection = require('./presets/shopify_introspection.json');
+const fluentIntrospection = require('./presets/fluent_introspection.json');
 
 export const PRESETS = {
-  'Star Wars': swapiIntrospection,
-  Yelp: yelpIntrospection,
-  'Shopify Storefront': shopifyIntrospection,
-  GitHub: githubIntrospection,
+  Fluent: fluentIntrospection,
 };
 
-export const defaultPresetName = 'Star Wars';
+export const defaultPresetName = 'Fluent';
 export const defaultPreset = PRESETS[defaultPresetName];
