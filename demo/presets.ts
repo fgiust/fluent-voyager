2d928340be04806e4f2ce4e7460c48a2d92bf2ae
fabrizio.giustina <fabrizio.giustina@accenture.com>
2023-06-02 11:19:54 +0200

    built pages

--- demo/presets.ts
+++
@@ -1,8 +0,0 @@
-const fluentIntrospection = require('./presets/fluent_introspection.json');
-
-export const PRESETS = {
-  Fluent: fluentIntrospection,
-};
-
-export const defaultPresetName = 'Fluent';
-export const defaultPreset = PRESETS[defaultPresetName];
